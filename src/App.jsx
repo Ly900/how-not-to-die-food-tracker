29338016e9e6a973ef39554e137262153d7afc2f
Ly Nguyen <lynguyendc@gmail.com>
2024-05-05 10:50:32 -0400

    Add button group

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,19 @@ function App() {
 	return (
 		<>
 			<Header />
+			<div className="tracker__options">
+				<p className="text-lg mb-2">
+					Choose how you want to import your desired foods:
+				</p>
+				<div className="tracker__option-buttons flex flex-col">
+					<button className="mb-3 inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-full">
+						Recommended
+					</button>
+					<button className="bg-blue-500 text-white py-2 px-4 rounded-full opacity-50 cursor-not-allowed">
+						Custom
+					</button>
+				</div>
+			</div>
 		</>
 	);
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ function Header() {
 			<h1 className="tracker__main-heading font-bold text-center text-5xl mb-2">
 				How Not To Die Food Tracker
 			</h1>
-			<p className="mb-3">
+			<p className="mb-3 text-2xl">
 				Use this app to track your monthly intake of healthy foods.
 			</p>
-			<img src="../src/assets/broccoli.svg" className="" />
+			<img src="../src/assets/broccoli.svg" className="mb-5" />
 		</header>
 	);
 }
