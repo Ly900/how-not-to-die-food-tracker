1ad56ac267149407bc055bf0c04afa012cfd3f39
Ly Nguyen <lynguyendc@gmail.com>
2024-05-05 09:43:48 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<h1 className="text-3xl font-bold text-red-500 text-center">
+				How Not To Die Food Tracker
+			</h1>
+		</>
+	);
+}
+
+export default App;
