529d61a5a1f73f9836206c32e39d68943c1c0db7
Ly Nguyen <lynguyendc@gmail.com>
2024-05-05 11:03:30 -0400

    Style buttons

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,11 @@ function App() {
 				<p className="text-lg mb-2">
 					Choose how you want to import your desired foods:
 				</p>
-				<div className="tracker__option-buttons flex flex-col">
-					<button className="mb-3 inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-full">
+				<div className="tracker__option-buttons inline-flex flex-col">
+					<button className="mb-3 inline-block bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded transition-colors text-lg antialiased font-medium uppercase">
 						Recommended
 					</button>
-					<button className="bg-blue-500 text-white py-2 px-4 rounded-full opacity-50 cursor-not-allowed">
+					<button className="bg-slate-300 text-black py-2 px-4 rounded opacity-50 cursor-not-allowed text-lg antialiased font-medium uppercase">
 						Custom
 					</button>
 				</div>
