034d2b4f1181d0303ee0039f724c71cce4b82521
Ly Nguyen <lynguyendc@gmail.com>
2024-05-05 11:48:54 -0400

    Style input

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 	return (
 		<>
 			<Header />
-			<div className="tracker__step0-container mb-3">
+			<div className="tracker__step0-container mb-4">
 				<button
 					className="mb-3 inline-block bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded transition-colors text-lg antialiased font-medium uppercase"
 					onClick={handleBeginClick}
@@ -19,22 +19,24 @@ function App() {
 				</button>
 			</div>
 
-			<div className="tracker__step1-container mb-3">
+			<div className="tracker__step1-container mb-4">
 				<p className="text-lg mb-2 font-bold">Step 1:</p>
 				<p className="text-lg mb-2">What month do you want to track?</p>
-				<label for="email" class="lu avz awd awo axu hidden">
-					Month:
-				</label>
-				<input
-					type="text"
-					name="month"
-					id="month"
-					className="text-center"
-					placeholder="May"
-				/>
+				<form>
+					<label for="email" class="lu avz awd awo axu hidden">
+						Month:
+					</label>
+					<input
+						type="text"
+						name="month"
+						id="month"
+						className="text-center border-b-2 border-lime-600"
+						placeholder="May"
+					/>
+				</form>
 			</div>
 
-			<div className="tracker__step2-container mb-3">
+			<div className="tracker__step2-container mb-4">
 				<p className="text-lg mb-2 font-bold">Step 2:</p>
 				<p className="text-lg mb-2">
 					Choose how you want to import your desired foods:
