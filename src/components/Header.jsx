4d91c8be98a6387e9a0fa007ebc3b2a47756d027
Ly Nguyen <lynguyendc@gmail.com>
2024-05-05 12:56:01 -0400

    Fix header margin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ import './Header.scss';
 
 function Header() {
 	return (
-		<header className="flex flex-col items-center">
+		<header className="flex flex-col items-center mb-5">
 			<h1 className="tracker__main-heading font-bold text-center text-5xl mb-2">
 				How Not To Die Food Tracker
 			</h1>
 			<p className="mb-3 text-2xl">
 				Use this app to track your monthly intake of healthy foods.
 			</p>
-			<img src="../src/assets/broccoli.svg" className="mb-8" />
+			<img src="../src/assets/broccoli.svg" className="" />
 		</header>
 	);
 }
