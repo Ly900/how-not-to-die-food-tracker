3d40262907c848a746fe67b0ab39a3e75f709cf2
Ly Nguyen <lynguyendc@gmail.com>
2024-05-05 21:22:18 -0400

    Create counter for Foods

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,13 +1,33 @@
+import { useState } from 'react';
 import Checkmark from './Checkmark';
 
 function Food({ food }) {
+	const [count, setCount] = useState(0);
+
+	function handleAddClick() {
+		setCount((count) => count + 1);
+	}
+
+	function handleRemoveClick() {
+		if (count === 0) {
+			return;
+		}
+		setCount((count) => count - 1);
+	}
+
 	return (
 		<div className="tracker__row border-b-2 mb-2 pb-1">
 			<div className="tracker__chart-buttons">
-				<button className="inline-block bg-green-500 hover:bg-green-700 text-white py-1/2 px-1 rounded transition-colors text-base antialiased font-medium uppercase mr-2">
+				<button
+					className="inline-block bg-green-500 hover:bg-green-700 text-white py-1/2 px-1 rounded transition-colors text-base antialiased font-medium uppercase mr-2"
+					onClick={() => handleAddClick()}
+				>
 					Add
 				</button>
-				<button className="inline-block bg-slate-500 hover:bg-slate-700 text-white py-1/2 px-1 rounded transition-colors text-base antialiased font-medium uppercase">
+				<button
+					className="inline-block bg-slate-500 hover:bg-slate-700 text-white py-1/2 px-1 rounded transition-colors text-base antialiased font-medium uppercase"
+					onClick={() => handleRemoveClick()}
+				>
 					Remove
 				</button>
 			</div>
@@ -17,8 +37,8 @@ function Food({ food }) {
 				</span>
 			</div>
 			<div className="flex align-middle">
-				<Checkmark />
-				<Checkmark />
+				{/* <Checkmark /> */}
+				{count}
 			</div>
 		</div>
 	);
