fe7a1f792d7f8b5c832e50855421e37ac021af66
Ly Nguyen <lynguyendc@gmail.com>
2024-05-05 21:27:45 -0400

    Style Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,14 @@ function Header({ step }) {
 				step === 'chart' ? 'mb-0' : 'mb-5'
 			}`}
 		>
-			<div className="flex">
+			<div className="flex mb-2">
 				<h1
 					className={`tracker__main-heading font-bold text-center text-5xl mb-0`}
 				>
 					<i className="text-green-700">How Not To Die</i> Food Tracker
 				</h1>
 			</div>
-			<p className="mb-3 text-2xl">
+			<p className="mb-3 text-2xl text-center w-5/6">
 				Use this app to track your monthly intake of healthy foods.
 			</p>
 			{step !== 'chart' && <img src="../src/assets/broccoli.svg" />}
