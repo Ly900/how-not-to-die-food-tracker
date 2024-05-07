adacf35c69a63b5dbc3e5fd1e28fcbada52669bb
Ly Nguyen <lynguyendc@gmail.com>
2024-05-06 20:42:44 -0400

    Add links to learm about How Not to Die

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,9 +14,42 @@ function Header({ step }) {
 					<i className="text-green-700">How Not To Die</i> Food Tracker
 				</h1>
 			</div>
-			<p className="mb-3 text-xl text-center w-5/6 mb-3">
+			<p className="mb-3 text-xl text-center w-5/6 mb-1">
 				Use this app to track your monthly intake of healthy foods.
 			</p>
+			<div>
+				<p className="mb-1 text-base text-center">
+					Learn more about How Not to Die at the following links:
+				</p>
+				<ul className="mb-3 text-base text-center flex gap-2 justify-center">
+					<li>
+						<a
+							href="https://www.amazon.com/How-Not-Die-Discover-Scientifically/dp/1250066115"
+							className="underline text-blue-600 hover:text-blue-800 font-medium"
+						>
+							Amazon.com
+						</a>{' '}
+						|
+					</li>
+					<li>
+						<a
+							href="https://nutritionfacts.org/book/how-not-to-die/"
+							className="underline text-blue-600 hover:text-blue-800 font-medium"
+						>
+							NutritionFacts.org
+						</a>{' '}
+						|
+					</li>
+					<li>
+						<a
+							href="https://www.goodreads.com/book/show/25663961-how-not-to-die"
+							className="underline text-blue-600 hover:text-blue-800 font-medium"
+						>
+							GoodReads
+						</a>
+					</li>
+				</ul>
+			</div>
 			{step !== 'chart' && (
 				<img src="/how-not-to-die-food-tracker/assets/broccoli.svg" />
 			)}
