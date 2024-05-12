3f2434b32be37a8a182be6a700dcc9685f2539fb
Ly Nguyen <lynguyendc@gmail.com>
2024-05-11 21:18:31 -0400

    Make notification its own component

---
+++ src/components/Notification.jsx
@@ -0,0 +1,38 @@
+function Notification({ displayNotification, food, month, negativeServings }) {
+	return (
+		<>
+			{displayNotification === 'addServing' && (
+				<p className="text-green-800">
+					Nice! You've added 1 serving of <strong>{food}</strong>.
+				</p>
+			)}
+			{displayNotification === 'removeServing' && !negativeServings && (
+				<p className="text-red-800">
+					You've removed 1 serving of <strong>{food}</strong>.
+				</p>
+			)}
+			{displayNotification === 'removeServing' && negativeServings && (
+				<p className="text-red-800">
+					You have no <strong>{food}</strong> to remove.
+				</p>
+			)}
+			{displayNotification === 'saveMonth' && (
+				<p className="text-green-800">
+					You've saved month <strong>{month}</strong>.
+				</p>
+			)}
+			{displayNotification === 'loadMonth' && (
+				<p className="text-green-800">
+					You've loaded month <strong>{month}</strong>.
+				</p>
+			)}
+			{displayNotification === 'deleteMonth' && (
+				<p className="text-red-800">
+					You've deleted month <strong>{month}</strong>.
+				</p>
+			)}
+		</>
+	);
+}
+
+export default Notification;
--- src/components/TrackerInstructions.jsx
+++ src/components/TrackerInstructions.jsx
@@ -1,56 +1,11 @@
+import Notification from './Notification';
+
 function TrackerInstructions({
 	food,
 	month,
 	negativeServings,
 	displayNotification,
 }) {
-	function getNotificationText() {
-		let notification;
-
-		if (displayNotification === 'addServing') {
-			notification = (
-				<p className="text-green-800">
-					Nice! You've added 1 serving of <strong>{food}</strong>.
-				</p>
-			);
-		} else if (displayNotification === 'removeServing') {
-			if (!negativeServings) {
-				notification = (
-					<p className="text-red-800">
-						You've removed 1 serving of <strong>{food}</strong>.
-					</p>
-				);
-			} else {
-				notification = (
-					<p className="text-red-800">
-						You have no <strong>{food}</strong> to remove.
-					</p>
-				);
-			}
-		} else if (displayNotification === 'saveMonth') {
-			notification = (
-				<p className="text-green-800">
-					You've saved month <strong>{month}</strong>.
-				</p>
-			);
-		} else if (displayNotification === 'deleteMonth') {
-			notification = (
-				<p className="text-red-800">
-					You've deleted month <strong>{month}</strong>.
-				</p>
-			);
-		} else if (displayNotification === 'loadMonth') {
-			notification = (
-				<p className="text-green-800">
-					You've loaded month <strong>{month}</strong>.
-				</p>
-			);
-		}
-		return notification;
-	}
-
-	const notification = getNotificationText();
-
 	return (
 		<div className="tracker__instructions-container p-3 text-center sm:text-left">
 			<div className="tracker__instructions mb-3 text-sm sm:text-base">
@@ -63,7 +18,12 @@ function TrackerInstructions({
 				</p>
 			</div>
 			<div className="tracker__notifications min-h-10 text-sm sm:text-base">
-				{notification}
+				<Notification
+					displayNotification={displayNotification}
+					food={food}
+					month={month}
+					negativeServings={negativeServings}
+				/>
 			</div>
 		</div>
 	);
