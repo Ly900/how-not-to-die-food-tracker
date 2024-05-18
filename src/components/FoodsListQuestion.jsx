862d9ac2027c066c34c7f878110d0ffd81595b59
Ly Nguyen <lynguyendc@gmail.com>
2024-05-18 12:10:42 -0400

    Fix styling

--- src/components/FoodsListQuestion.jsx
+++ src/components/FoodsListQuestion.jsx
@@ -1,9 +1,9 @@
 function FoodsListQuestion({ handleDailyDozenClick, handleCustomClick }) {
 	return (
-		<div className="tracker__step2-container bg-slate-100 pt-3 pb-6 text-lg">
+		<div className="tracker__step2-container bg-slate-100 p-2 pb-6 text-lg">
 			<p className="mb-2 font-bold">Step 2:</p>
 			<p className="mb-2">Choose how you want to import your desired foods:</p>
-			<div className="tracker__option-buttons inline-flex flex-col">
+			<div className="tracker__option-buttons inline-flex flex-col ">
 				<button
 					className="mb-3 inline-block bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded transition-colors text-lg antialiased font-medium uppercase"
 					onClick={() => handleDailyDozenClick()}
