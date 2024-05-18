e308fb8ae7a49e4e7e2a9aea8ca9e8ad3aad5c14
Ly Nguyen <lynguyendc@gmail.com>
2024-05-18 12:04:42 -0400

    Fix foods list container padding

--- src/components/FoodsListQuestion.jsx
+++ src/components/FoodsListQuestion.jsx
@@ -1,9 +1,9 @@
 function FoodsListQuestion({ handleDailyDozenClick, handleCustomClick }) {
 	return (
-		<div className="tracker__step2-container bg-slate-100 p-4 pb-6 text-lg">
+		<div className="tracker__step2-container bg-slate-100 pt-3 pb-6 text-lg">
 			<p className="mb-2 font-bold">Step 2:</p>
 			<p className="mb-2">Choose how you want to import your desired foods:</p>
-			<div className="tracker__option-buttons inline-flex flex-col mb-3">
+			<div className="tracker__option-buttons inline-flex flex-col">
 				<button
 					className="mb-3 inline-block bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded transition-colors text-lg antialiased font-medium uppercase"
 					onClick={() => handleDailyDozenClick()}
@@ -11,7 +11,7 @@ function FoodsListQuestion({ handleDailyDozenClick, handleCustomClick }) {
 					Daily Dozen
 				</button>
 				<button
-					className="mb-3 inline-block bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded transition-colors text-lg antialiased font-medium uppercase"
+					className="mb-0 inline-block bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded transition-colors text-lg antialiased font-medium uppercase"
 					onClick={() => {
 						handleCustomClick();
 					}}
